37bb3a2c778ed6bef36d068ddde72c99d7f195dc
Shirley Xiao <xiaoshirl@gmail.com>
2020-11-06 22:50:35 -0800

    protected token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const config = require("./config.json");
 const client = new Discord.Client();
 
 fs.readdir("./events/", (err, files) => {  
@@ -15,51 +16,66 @@ const permInteger = '271961121';
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-})
-client.on("message", (msg) => {
-  if (msg.content === "hello") {
-    msg.reply("hey!")
-  }
-})
-
-
-client.on("guildMemberAdd", (member) => {
-  member.send(
-    `Welcome to the Ascend server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
-  )
-})
-
-
-client.on("message", (message) => {
-  if (message.content.startsWith("!kick")) {
-    const member = message.mentions.members.first()
-    if (!member) {
-      return message.reply(
-        `Who are you trying to kick? You must mention a user.`
-      )
-    }
-    if (!member.kickable) {
-      return message.reply(`I can't kick this user. Sorry!`)
-    }
-    return member
-      .kick()
-      .then(() => message.reply(`${member.user.tag} was kicked.`))
-      .catch((error) => message.reply(`Sorry, an error occured.`))
-  }
-})
-
-
-let counter = 0;
+}) 
 
 //event listeners
-client.on(`message`, (receivedMessage) => 
-{
-    if (receivedMessage.channel.id == '756683017233235989') { // change channel id
-      count(receivedMessage);
-}
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  // greeting test
+  client.on("message", (msg) => {
+    if (msg.content === "test") {
+      msg.reply("hey!")
+    }
+  })
+
+  // new member welcome
+  client.on("guildMemberAdd", (member) => {
+    member.send(
+      `Welcome to the Ascend server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`
+    )
+  })
+
+  // kick member 
+  client.on("message", (message) => {
+    // help
+    if (message.content.startsWith(config.prefix + "help")) {
+      return message.reply('commands: !')
+    }
+    
+    // kick 
+    if (message.content.startsWith(config.prefix + "kick")) {
+      const member = message.mentions.members.first()
+      if (!member) {
+        return message.reply(`Who are you trying to kick? You must mention a user.`)
+      }
+      if (!member.kickable) {
+        return message.reply(`I can't kick this user. Sorry!`)
+      }
+      return member
+        .kick()
+        .then(() => message.reply(`${member.user.tag} was kicked.`))
+        .catch((error) => message.reply(`Sorry, an error occured.`))
+    }
+  })
 });
 
-function count(receivedMessage) {
-    counter++;
-    receivedMessage.channel.send(counter);
-}
+
+
+
+
+// message counter
+// let counter = 0;
+
+// client.on(`message`, (receivedMessage) => 
+// {
+//     if (receivedMessage.channel.id == '756683017233235989') { // change channel id
+//       count(receivedMessage);
+// }
+// });
+
+// function count(receivedMessage) {
+//     counter++;
+//     receivedMessage.channel.send(counter);
+// }
