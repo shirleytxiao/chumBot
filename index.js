96f4fca43234424b0390fe4c19fbc1d2c6bc7f02
Shirley Xiao <xiaoshirl@gmail.com>
2020-11-06 21:57:15 -0800

    protected token

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
+  if (msg.content === "hello") {
+    msg.reply("hey!")
   }
 })
 
