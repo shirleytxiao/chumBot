a0dd10952e0edce0a67ed90f0a1d86a211bd2b5e
Shirley Xiao <xiaoshirl@gmail.com>
2020-12-04 14:28:45 -0800

    removed fs.readdir

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const config = require("./config.json");
 const bot = new Discord.Client();
 
-fs.readdir("./events/", (err, files) => {  
-  files.forEach((file) => {
-    const eventHandler = require(`./events/${file}`);
-    const eventName = file.split(".")[0];
-    bot.on(eventName, (...args) => eventHandler(bot, ...args));
-;})});
+// fs.readdir("./events/", (err, files) => {  
+//   files.forEach((file) => {
+//     const eventHandler = require(`./events/${file}`);
+//     const eventName = file.split(".")[0];
+//     bot.on(eventName, (...args) => eventHandler(bot, ...args));
+// ;})});
 
 bot.login(config.token);
 
