d5a44d405db8c5e1bf65f1c37459725170303c99
Shirley Xiao <xiaoshirl@gmail.com>
2020-12-04 15:27:44 -0800

    added music play command

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const Discord = require("discord.js");
-const fs = require("fs");
+const ytdl = require("ytdl-core");
 const config = require("./config.json");
 const bot = new Discord.Client();
-
-// fs.readdir("./events/", (err, files) => {  
-//   files.forEach((file) => {
-//     const eventHandler = require(`./events/${file}`);
-//     const eventName = file.split(".")[0];
-//     bot.on(eventName, (...args) => eventHandler(bot, ...args));
-// ;})});
+const queue = new Map();
 
 bot.login(config.token);
 
@@ -39,14 +33,14 @@ bot.on("message", (message) => {
 
   // help
   if (command === "help") {
-    return message.reply('commands: \n!hi   --- greeting \n!kick <user>   --- kick user out of server \n!aboutme <1/2/3/4> <location> <committee>   --- greets you by your year (1, 2, 3, or 4), location, and committee.')
+    return message.reply('commands: \n\n!hi   --- greeting \n\n!kick <user>   --- kick user out of server \n\n!aboutme <1/2/3/4> <location> <committee>   --- greets you by your year (1, 2, 3, or 4), location, and committee.')
   }
   
   // kick 
   if (command === "kick") {
     let member = message.mentions.members.first();      
     if (!member) {
-      return message.reply(`Who are you trying to kick? You must mention a user.`)
+      return message.reply(`who are you trying to kick? You must mention a user.`)
     }
     if (!member.kickable) {
       return message.reply(`I can't kick this user. Sorry!`)
@@ -55,7 +49,7 @@ bot.on("message", (message) => {
     return member
       .kick(reason)
       .then(() => message.reply(`${member.user.tag} was kicked.`))
-      .catch((error) => message.reply(`Sorry, an error occured.`))
+      .catch((error) => message.reply(`sorry, an error occured.`))
   }
 
   // aboutme: year, location, committee
@@ -76,7 +70,105 @@ bot.on("message", (message) => {
         year_name = "senior";
         break;
     }
-    return message.reply(`Hello ${message.author.username}, I see you're a ${year_name} from ${location} in the ${committee} committee.`);
+    return message.reply(`I see you're a ${year_name} from ${location} in the ${committee} committee.`);
+  }
+
+  // play music
+  if (command === "play") {
+    execute(message, serverQueue);
+    return;
+  } else if (command === "skip") {
+    skip(message, serverQueue);
+    return;
+  } else if (command === "stop") {
+    stop(message, serverQueue);
+    return;
+  }
+
+  async function execute(message, serverQueue) {  
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.channel.send(
+        "You need to be in a voice channel to play music!"
+      );
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+      return message.channel.send(
+        "I need the permissions to join and speak in your voice channel!"
+      );
+    }
+
+    const songInfo = ytdl.getInfo(args[1]);
+    const song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+     };
+    
+    if (!serverQueue) {
+      const queueContruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      };
+
+      queue.set(message.guild.id, queueContruct);
+
+      queueContruct.songs.push(song);
+
+      try {
+        var connection = voiceChannel.join();
+        queueContruct.connection = connection;
+        play(message.guild, queueContruct.songs[0]);
+      } catch (err) {
+        console.log(err);
+        queue.delete(message.guild.id);
+        return message.channel.send(err);
+      }
+    } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`${song.title} has been added to the queue!`);
+    }
+  }
+
+  function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+  }
+
+  function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
+
+  function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    if (!song) {
+      serverQueue.voiceChannel.leave();
+      queue.delete(guild.id);
+      return;
+    }
+  
+    const dispatcher = serverQueue.connection
+      .play(ytdl(song.url))
+      .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+      })
+      .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
   }
 
   // event: create event & generate google calendar invite
